8332df6c0bb80003424426d4bfe27180a27520a4
lucalewin <luca.lewin23@gmail.com>
2021-01-22 00:57:34 +0100

    create default Form

--- AutoClicker/Form1.Designer.cs
+++ AutoClicker/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace AutoClicker
+﻿using System.Drawing;
+
+namespace AutoClicker
 {
     partial class Form1
     {
@@ -17,24 +19,194 @@
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.checkBoxEnable = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemView = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemViewPreferences = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // checkBoxEnable
+            // 
+            this.checkBoxEnable.AutoSize = true;
+            this.checkBoxEnable.Location = new System.Drawing.Point(12, 165);
+            this.checkBoxEnable.Name = "checkBoxEnable";
+            this.checkBoxEnable.Size = new System.Drawing.Size(147, 19);
+            this.checkBoxEnable.TabIndex = 3;
+            this.checkBoxEnable.Text = "Enabled (Shortcut [F4])";
+            this.checkBoxEnable.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(12, 190);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(108, 19);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Always focused";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckBoxAlwaysFocused_CheckedChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemFile,
+            this.menuItemView,
+            this.menuItemAbout});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(324, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuItemFile
+            // 
+            this.menuItemFile.Name = "menuItemFile";
+            this.menuItemFile.Size = new System.Drawing.Size(37, 20);
+            this.menuItemFile.Text = "File";
+            // 
+            // menuItemView
+            // 
+            this.menuItemView.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemViewPreferences});
+            this.menuItemView.Name = "menuItemView";
+            this.menuItemView.Size = new System.Drawing.Size(44, 20);
+            this.menuItemView.Text = "View";
+            // 
+            // menuItemViewPreferences
+            // 
+            this.menuItemViewPreferences.Name = "menuItemViewPreferences";
+            this.menuItemViewPreferences.Size = new System.Drawing.Size(135, 22);
+            this.menuItemViewPreferences.Text = "Preferences";
+            // 
+            // menuItemAbout
+            // 
+            this.menuItemAbout.CheckOnClick = true;
+            this.menuItemAbout.Name = "menuItemAbout";
+            this.menuItemAbout.Size = new System.Drawing.Size(52, 20);
+            this.menuItemAbout.Text = "About";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 56);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(167, 23);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(7, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Interval between clicks";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 86);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 19);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Seconds";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(88, 86);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 19);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Milliseconds";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.ClientSize = new System.Drawing.Size(324, 459);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBoxEnable);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.CheckBox checkBoxEnable;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem menuItemView;
+        private System.Windows.Forms.ToolStripMenuItem menuItemAbout;
+        private System.Windows.Forms.ToolStripMenuItem menuItemViewPreferences;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
-
--- AutoClicker/Form1.cs
+++ AutoClicker/Form1.cs
@@ -1,21 +1,146 @@
-﻿using System;
+﻿using AutoClicker.Hooks;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AutoClicker
 {
-    public partial class Form1 : Form
+    public partial class Form1 : System.Windows.Forms.Form
     {
+        private readonly MouseHook mouseHook = new();
+        private readonly KeyboardHook keyHook = new();
+        #region SimulateLeftClick
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+        public static void PerformLeftClick(int xpos, int ypos)
+        {
+            //SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            //mouseLeftCount++;
+            //mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+        public static void PerformRightClick(int xpos, int ypos)
+        {
+            //SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, xpos, ypos, 0, 0);
+            //mouseRightCount++;
+            //mouse_event(MOUSEEVENTF_RIGHTUP, xpos, ypos, 0, 0);
+        }
+        #endregion
+        private void mouseMove(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            mouseX = mouseStruct.pt.x;
+            mouseY = mouseStruct.pt.y;
+        }
+        int mouseX;
+        int mouseY;
+        //static int mouseLeftCount = 0;
+        //static int mouseRightCount = 0;
+        bool leftDown = false;
+        bool rightDown = false;
+        DateTime timeLeftClicked = DateTime.Now;
+        DateTime timeRightClicked = DateTime.Now;
+
         public Form1()
         {
             InitializeComponent();
+            string title =
+                $"Lucraft.AutoClicker v{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion?.Split("+")[0]}";
+            Name = title;
+            Text = title;
+
+            backgroundWorker1.RunWorkerAsync();
+
+
+            mouseHook.MouseMove += new MouseHook.MouseHookCallback(mouseMove);
+            mouseHook.LeftButtonDown += new MouseHook.MouseHookCallback(leftMouseDown);
+            mouseHook.LeftButtonUp += new MouseHook.MouseHookCallback(leftMouseUp);
+            mouseHook.RightButtonDown += new MouseHook.MouseHookCallback(rightMouseDown);
+            mouseHook.RightButtonUp += new MouseHook.MouseHookCallback(rightMouseUp);
+            keyHook.KeyUp += new KeyboardHook.KeyboardHookCallback(keyUp);
+            mouseHook.Install();
+            keyHook.Install();
         }
 
+        private void keyUp(KeyboardHook.VKeys key)
+        {
+            if (key == KeyboardHook.VKeys.OEM_102)
+            {
+                checkBoxEnable.Checked = !checkBoxEnable.Checked;
+            }
+        }
+
+        private void rightMouseDown(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            if (rightDown == false)
+            {
+                timeRightClicked = DateTime.Now;
+            }
+            rightDown = true;
+        }
+
+        private void rightMouseUp(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            rightDown = false;
+        }
+
+        private void leftMouseUp(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            leftDown = false;
+        }
+        private void leftMouseDown(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            if (leftDown == false)
+            {
+                timeLeftClicked = DateTime.Now;
+            }
+            leftDown = true;
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            do
+            {
+                lock (this)
+                {
+                    if (checkBoxEnable.Checked)
+                    {
+                        if (leftDown && (int)DateTime.Now.Subtract(timeLeftClicked).TotalMilliseconds > 20)
+                        {
+                            PerformLeftClick(mouseX, mouseY);
+                            Thread.Sleep((int)10);
+                        }
+                        else if (rightDown && (int)DateTime.Now.Subtract(timeRightClicked).TotalMilliseconds > 20)
+                        {
+                            PerformRightClick(mouseX, mouseY);
+                            Thread.Sleep((int)10);
+                        }
+                        //lblLeftClicks.Text = mouseLeftCount.ToString();
+                        //lblRightClicks.Text = mouseRightCount.ToString();
+                    }
+
+                }
+
+            } while (true);
+        }
+
+        private void CheckBoxAlwaysFocused_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = !TopMost;
+        }
     }
 }
