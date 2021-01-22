6273e9687d67c08d1eea852658eb67eafe6adf2e
lucalewin <luca.lewin23@gmail.com>
2021-01-22 21:42:16 +0100

    adding Themes to support Light + Darkmode

---
+++ AutoClicker/Themes.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace AutoClicker
+{
+    internal enum Theme
+    {
+        Dark = 0,
+        Light = 1
+    }
+
+    internal static class Themes
+    {
+        public static Theme CurrentTheme()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return (Theme)(int)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", "AppsUseLightTheme", 1);
+            return Theme.Light;
+        }
+    }
+}
